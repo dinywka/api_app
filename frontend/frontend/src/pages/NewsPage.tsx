a33c5c19e49b9c70b9f6ad07753b8aa5cc92432c
Dina <73873857+dinywka@users.noreply.github.com>
2023-09-15 20:24:32 +0600

    second

--- frontend/frontend/src/pages/NewsPage.tsx
+++ frontend/frontend/src/pages/NewsPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import * as bases from "../components/ui/bases";
 import axios from "axios";
 
 interface NewsItem {
@@ -22,19 +23,21 @@ export default function NewsPage() {
   }, []);
 
   return (
-    <div className={"container p-3"}>
-      <h2>News</h2>
-      {newsData.length === 0 ? (
-        <p>No news available.</p>
-      ) : (
-        <ul>
-          {newsData.map((newsItem, index) => (
-            <li key={index}>
-              {newsItem.title}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+      <bases.Base1>
+        <div className={"container p-3"}>
+          <h2>News</h2>
+          {newsData.length === 0 ? (
+            <p>No news available.</p>
+          ) : (
+            <ul>
+              {newsData.map((newsItem, index) => (
+                <li key={index}>
+                  {newsItem.title}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        </bases.Base1>
   );
 }
